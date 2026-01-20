894d8b509963836e5a3ee628be98e023349225c3
Zixu Cheng <acw761@qmul.ac.uk>
2026-01-20 13:10:49 +0000

    update OA

---
+++ ECS795P_CW1OA/asset/student_list_test.js
@@ -0,0 +1,527 @@
+var students = [
+    {
+        "Full name": "TEST Test",
+        "ID number": "88888888",
+        "Email address": "2022@test.qmul.ac.uk",
+        "Question": "2.3",
+        "Exercise": "2.3"
+    },
+    {
+        "Full name":"Meryem Sena \u00bfiltu",
+        "ID number":"220886244",
+        "Email address":"m.s.siltu@se22.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":"Simrat Singh .",
+        "ID number":"230719732",
+        "Email address":"simrat.singh@se23.qmul.ac.uk",
+        "Question":"3.3",
+        "Exercise":"1.1"
+    },
+    {
+        "Full name":"Abdulmalik Aldawsari",
+        "ID number":"230930647",
+        "Email address":"a.aldawsari@se23.qmul.ac.uk",
+        "Question":"2.3",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Abukar Mohamed Ali",
+        "ID number":"160936205",
+        "Email address":"a.ali@se23.qmul.ac.uk",
+        "Question":"2.2",
+        "Exercise":"1.1"
+    },
+    {
+        "Full name":"Mohammed Othman K Almelabi",
+        "ID number":"230564774",
+        "Email address":"m.almelabi@se23.qmul.ac.uk",
+        "Question":"2.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Falah Hazeem Khalifa Ali Alsuwaidi",
+        "ID number":"221041938",
+        "Email address":"f.alsuwaidi@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Chris J Anandha Sundara Raj Vijayshree",
+        "ID number":"231096360",
+        "Email address":"c.anandhasundararajvijayshree@se23.qmul.ac.uk",
+        "Question":"3.3",
+        "Exercise":"3.3"
+    },
+    {
+        "Full name":"Divya Anbalagan Nadar",
+        "ID number":"230939640",
+        "Email address":"d.nadar@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Sanmay Santosh Avhad",
+        "ID number":"230311444",
+        "Email address":"s.avhad@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"3.5"
+    },
+    {
+        "Full name":"Aman Bisht",
+        "ID number":"231046365",
+        "Email address":"a.bisht@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"3.2"
+    },
+    {
+        "Full name":"Carey David Bunks",
+        "ID number":"220319144",
+        "Email address":"c.bunks@qmul.ac.uk",
+        "Question":"2.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Rayyan Masood Chagani",
+        "ID number":"190866419",
+        "Email address":"r.chagani@se19.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Wei-Han Chen",
+        "ID number":"230249354",
+        "Email address":"w.chen@se23.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Tinevimbo Chitando",
+        "ID number":"230749913",
+        "Email address":"t.chitando@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Vimanyu Devgan",
+        "ID number":"230504736",
+        "Email address":"v.devgan@se23.qmul.ac.uk",
+        "Question":"3.2",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Yunfei Du",
+        "ID number":"230726707",
+        "Email address":"y.du@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Fatemeh-Eva Ekhteyary",
+        "ID number":"220911155",
+        "Email address":"f.ekhteyary@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Iliass El Yaakoubi Benssaleh",
+        "ID number":"200922847",
+        "Email address":"i.elyaakoubibenssaleh@se20.qmul.ac.uk",
+        "Question":"3.2",
+        "Exercise":"1.4"
+    },
+    {
+        "Full name":"Christopher Michael Evans",
+        "ID number":"231020435",
+        "Email address":"c.m.evans@se23.qmul.ac.uk",
+        "Question":"3.4",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":"Xuanye Fei",
+        "ID number":"230845581",
+        "Email address":"x.fei@hss22.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Bernhard Sven Finke",
+        "ID number":"230872255",
+        "Email address":"b.s.finke@qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"3.4"
+    },
+    {
+        "Full name":"Kaviraj Gosaye",
+        "ID number":"220575371",
+        "Email address":"k.gosaye@se23.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Naveen  Raj Govindaraj",
+        "ID number":"220829285",
+        "Email address":"n.govindaraj@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Nari Ha",
+        "ID number":"230195741",
+        "Email address":"n.ha@se23.qmul.ac.uk",
+        "Question":"3.5",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Kiana Hadysadegh",
+        "ID number":"230632224",
+        "Email address":"k.hadysadegh@se23.qmul.ac.uk",
+        "Question":"2.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Kavya Shree Hebbaralu Suresh",
+        "ID number":"230382019",
+        "Email address":"k.hebbaralusuresh@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"1.1"
+    },
+    {
+        "Full name":"Tanisha Hisariya",
+        "ID number":"220929356",
+        "Email address":"t.hisariya@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Peijun Huang",
+        "ID number":"231042703",
+        "Email address":"peijun.huang@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"3.5"
+    },
+    {
+        "Full name":"Mohammad Tahidul Islam",
+        "ID number":"180567302",
+        "Email address":"m.islam@se23.qmul.ac.uk",
+        "Question":"2.3",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":"Aditya Mohan Iyer",
+        "ID number":"230892301",
+        "Email address":"a.iyer@se23.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":"Qiao Jing",
+        "ID number":"230858503",
+        "Email address":"q.jing@hss22.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Anu Kakkanattu Jose",
+        "ID number":"230532382",
+        "Email address":"a.kakkanattujose@se23.qmul.ac.uk",
+        "Question":"3.2",
+        "Exercise":"1.4"
+    },
+    {
+        "Full name":"Omar Masud Kalam",
+        "ID number":"170277943",
+        "Email address":"o.m.kalam@se17.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Indhu Kamanakere Hundi Nataraju",
+        "ID number":"230505342",
+        "Email address":"i.kamanakerehundinataraju@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Lavanya Kebbhalli Muruli",
+        "ID number":"230383359",
+        "Email address":"l.kebbhallimuruli@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Li Heng Khor",
+        "ID number":"230689365",
+        "Email address":"l.khor@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Thoithoi Chanu Kshetrimayum",
+        "ID number":"230882922",
+        "Email address":"t.kshetrimayum@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Bhavya Kumawat",
+        "ID number":"180524419",
+        "Email address":"b.kumawat@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"1.1"
+    },
+    {
+        "Full name":"Hyowon Lee",
+        "ID number":"230861592",
+        "Email address":"h.lee@hss23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Zemin Li",
+        "ID number":"230437984",
+        "Email address":"z.li@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Yi-Jou Lin",
+        "ID number":"230362088",
+        "Email address":"y.lin@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Zeliang Liu",
+        "ID number":"221182389",
+        "Email address":"zeliang.liu@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"3.2"
+    },
+    {
+        "Full name":"Piyush Shashank Lohokare",
+        "ID number":"230238819",
+        "Email address":"p.lohokare@se23.qmul.ac.uk",
+        "Question":"2.1",
+        "Exercise":"1.2"
+    },
+    {
+        "Full name":"Haoze Lu",
+        "ID number":"200943233",
+        "Email address":"h.lu@se20.qmul.ac.uk",
+        "Question":"2.3",
+        "Exercise":"3.1"
+    },
+    {
+        "Full name":"Tianxiu Ma",
+        "ID number":"230277302",
+        "Email address":"t.ma@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"3.5"
+    },
+    {
+        "Full name":"Mouneer Ahmad Osman Mahomed",
+        "ID number":"230889219",
+        "Email address":"m.a.o.mahomed@se23.qmul.ac.uk",
+        "Question":"2.3",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Renee Maria Mendonca",
+        "ID number":"221040908",
+        "Email address":"r.mendonca@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"1.2"
+    },
+    {
+        "Full name":"Michail Panagopoulos",
+        "ID number":"231083647",
+        "Email address":"m.panagopoulos@se23.qmul.ac.uk",
+        "Question":"3.3",
+        "Exercise":"1.2"
+    },
+    {
+        "Full name":"Michael Krasniqi Peres",
+        "ID number":"200362146",
+        "Email address":"m.peres@se20.qmul.ac.uk",
+        "Question":"3.4",
+        "Exercise":"1.1"
+    },
+    {
+        "Full name":"Ivan Yang Rodriguez Carranza",
+        "ID number":"230943463",
+        "Email address":"i.y.rodriguezcarranza@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":" Ruthwik Ganesh Mangalore Dayananda",
+        "ID number":"230702930",
+        "Email address":"r.-@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Rajdeep Roshan Sahu",
+        "ID number":"230215119",
+        "Email address":"r.sahu@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Srivarshini Senthil Kumar",
+        "ID number":"230788462",
+        "Email address":"s.senthilkumar@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"1.4"
+    },
+    {
+        "Full name":"Shahryar Shahbazijalalifarahani",
+        "ID number":"230883284",
+        "Email address":"s.shahbazijalalifarahani@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"1.2"
+    },
+    {
+        "Full name":"Tasmia Shakerin",
+        "ID number":"231007872",
+        "Email address":"t.shakerin@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Aditya Sharma",
+        "ID number":"221005424",
+        "Email address":"aditya.sharma@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Mohamed Ayaan Sheikh",
+        "ID number":"230068294",
+        "Email address":"m.a.sheikh@se23.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"1.3"
+    },
+    {
+        "Full name":"Rajan Siddhant",
+        "ID number":"231038429",
+        "Email address":"r.siddhant@se23.qmul.ac.uk",
+        "Question":"3.2",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Laxman Aadithya Singanalluri Venkatakrishna",
+        "ID number":"230166682",
+        "Email address":"l.singanallurivenkatakrishna@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Harjinder Singh",
+        "ID number":"231006602",
+        "Email address":"h.singh@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"1.4"
+    },
+    {
+        "Full name":"Vaishali Singh",
+        "ID number":"230710935",
+        "Email address":"v.singh@se23.qmul.ac.uk",
+        "Question":"3.5",
+        "Exercise":"3.5"
+    },
+    {
+        "Full name":"Mamta Siyak",
+        "ID number":"220027180",
+        "Email address":"m.siyak@se23.qmul.ac.uk",
+        "Question":"3.5",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Nikita Sunil Somani",
+        "ID number":"230828296",
+        "Email address":"n.s.somani@se23.qmul.ac.uk",
+        "Question":"2.4",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Stylianos Soteriou",
+        "ID number":"231055633",
+        "Email address":"s.soteriou@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"3.3"
+    },
+    {
+        "Full name":"Sophie Elin Strawbridge",
+        "ID number":"230990744",
+        "Email address":"s.strawbridge@se23.qmul.ac.uk",
+        "Question":"3.1",
+        "Exercise":"3.6"
+    },
+    {
+        "Full name":"Jacub Mateusz Szalacha",
+        "ID number":"200535773",
+        "Email address":"j.m.szalacha@se23.qmul.ac.uk",
+        "Question":"2.3",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Ziad Mazen M Tamim",
+        "ID number":"230973161",
+        "Email address":"z.tamim@se23.qmul.ac.uk",
+        "Question":"1.2",
+        "Exercise":"2.1"
+    },
+    {
+        "Full name":"Anthony Breeganzo Thomas",
+        "ID number":"230009529",
+        "Email address":"a.thomas@se23.qmul.ac.uk",
+        "Question":"3.5",
+        "Exercise":"2.3"
+    },
+    {
+        "Full name":"Shamna Mol Uthakkaden",
+        "ID number":"231090689",
+        "Email address":"s.uthakkaden@se23.qmul.ac.uk",
+        "Question":"3.3",
+        "Exercise":"3.4"
+    },
+    {
+        "Full name":"Bartosz Michal Watrobinski",
+        "ID number":"200496278",
+        "Email address":"b.watrobinski@se20.qmul.ac.uk",
+        "Question":"3.5",
+        "Exercise":"3.3"
+    },
+    {
+        "Full name":"Shu Ting Wong",
+        "ID number":"230221855",
+        "Email address":"s.wong@se23.qmul.ac.uk",
+        "Question":"3.2",
+        "Exercise":"2.2"
+    },
+    {
+        "Full name":"Hongjie Wu",
+        "ID number":"231145451",
+        "Email address":"h.wu@se23.qmul.ac.uk",
+        "Question":"2.5",
+        "Exercise":"3.3"
+    },
+    {
+        "Full name":"Jing Yu",
+        "ID number":"231098364",
+        "Email address":"j.yu@se23.qmul.ac.uk",
+        "Question":"1.1",
+        "Exercise":"1.4"
+    },
+    {
+        "Full name":"Lujie Zhang",
+        "ID number":"230766242",
+        "Email address":"l.zhang@se23.qmul.ac.uk",
+        "Question":"1.3",
+        "Exercise":"1.3"
+    }
+]
